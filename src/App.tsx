1f6eb5d7f1722216b52705e69cb1ba33782e956f
Szabó <szabo.beci10@gmail.com>
2024-03-03 16:50:37 +0100

    test_commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Fooldal } from './Pages/MainPage'
+
+function App() {
+
+
+  return (
+   <Fooldal/>
+  )
+}
+
+export default App
---
+++ src/Pages/MainPage.tsx
@@ -0,0 +1,52 @@
+export function Fooldal(){
+    return<div className="container">
+        <div className="hero-section">
+            <h1>
+                BeerCycle
+            </h1>
+            <button>
+                Fogalalj
+            </button>
+        </div>
+        <div className="second-section">
+            <h2>
+                Rólunk
+            </h2>
+            <ul>
+                <li>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, alias. Quidem, ipsa quaerat deserunt repudiandae obcaecati adipisci reprehenderit repellat numquam pariatur, eaque ratione enim porro iste placeat dolore odio aperiam!
+                </li>
+                <li>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, alias. Quidem, ipsa quaerat deserunt repudiandae obcaecati adipisci reprehenderit repellat numquam pariatur, eaque ratione enim porro iste placeat dolore odio aperiam!
+                </li>
+                <li>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, alias. Quidem, ipsa quaerat deserunt repudiandae obcaecati adipisci reprehenderit repellat numquam pariatur, eaque ratione enim porro iste placeat dolore odio aperiam!
+                </li>
+            </ul>
+        </div>
+        <div className="review-section">
+            <h2>
+                Visszajelzések
+            </h2>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, odit error aliquid velit impedit soluta exercitationem eaque repudiandae dolorem, consequatur optio possimus iusto harum cupiditate minus nihil hic distinctio facere!
+            </p>
+        </div>
+        <div className="mobil-section">
+            <h2>
+                Töltsd le mobil alkalmazásunkat!
+            </h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis explicabo beatae, ex quam, facilis ea voluptates rerum, eaque dolore quis distinctio qui modi nesciunt aperiam itaque ut esse! Reiciendis, est!
+            </p>
+            <button>
+                Letöltöm!
+            </button>
+        </div>
+        <div className="footer-section">
+            <p>
+                asd
+            </p>
+        </div>
+    </div>
+}
