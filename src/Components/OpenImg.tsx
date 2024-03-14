25e19ef6b61c243375d063707dadc85e7d02d64b
Szabó <szabo.beci10@gmail.com>
2024-03-14 21:51:24 +0100

    Galléria

---
+++ src/Components/OpenImg.tsx
@@ -0,0 +1,7 @@
+
+export function OpenImg(img:string)
+{
+    return(
+        
+    )
+}
--- src/Pages/GalleryPage.tsx
+++ src/Pages/GalleryPage.tsx
@@ -1,10 +1,27 @@
+import { Container, Grid, makeStyles } from "@mui/material";
 import Drawer from "@mui/material/Drawer";
 
-export function GalleryPage()
-{
-    return(
-        <>
-        <h1>Gallery Page</h1>
-        </>
+export function GalleryPage() {
+    let selfi = 'selfi ';
+    const selfiArray = [];
+    for (let index = 1; index < 16; index++) {
+        selfiArray.push(selfi + `(${index}).jpg`);
+    }
+
+    return (
+        <Container >
+        <Grid container justifyContent="center" spacing={4} sx={{mt:15}}>
+            {selfiArray.map((selfiImage, index) => (
+                <Grid item xs={6} sm={3} md={3} lg={3} key={index} >
+                    <img style={{
+                    margin: '4px',
+                    maxWidth: '100%',
+                    height: 'auto',
+                    display: 'block',
+                }} src={`src/Images/beerselfi/${selfiImage}`} alt={`Selfi ${index}`} />
+                </Grid>
+            ))}
+        </Grid>
+        </Container>
     )
 }
